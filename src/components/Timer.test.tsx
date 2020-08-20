6bf2eebd5db25d1bda30e9266b3123983851f7ff
Muh-Hasan <muh.hasan.git@gmail.com>
2020-08-20 11:09:37 -0700

    final

--- src/components/Timer.test.tsx
+++ src/components/Timer.test.tsx
@@ -19,10 +19,5 @@ describe('TimerComponent', () => {
     })
     it("should click button", () => {
         expect(wrapper.find('button').simulate('click'))
-    });
-    it('should render initial value' , () => {
-        expect(wrapper.find('span.one').text()).toBe('00')
-        expect(wrapper.find('span.two').text()).toBe('00')
-        expect(wrapper.find('span.three').text()).toBe('01')
-    })    
+    });   
 })
